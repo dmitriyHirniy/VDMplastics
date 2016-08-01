ddf32aa77e0b4fdc44169328e19b6062cc9dd152
Dima Khirniy <DmitriyKhirniy@gmail.com>
2016-08-01 23:14:34 +0300

    Section products of polypropylene is started

--- app/sections/polypropylene-products.component.ts
+++ app/sections/polypropylene-products.component.ts
@@ -7,11 +7,13 @@ class Row{
     ){}
 }
 
-class TableRow{
+class Product{
     constructor(
         public title: string,
         public price: string,
-        public weight: string
+        public description: string,
+        public pageLink: string,
+        public photo: string
     ){};
 }
 
@@ -24,10 +26,40 @@ class TableRow{
 export class PolypropyleneProductsComponent implements OnInit{
 
 
+    public products: Product[];
+
     public ngOnInit(){
+
+        this.products = [
+            new Product(
+                "Басейны",
+                "Уточнайте",
+                "Листовой полипропилен — экологически чистый, прочный и устойчивый к ультрафиолетовому излучению пластик",
+                "",
+                "http://aplast.com.ua/images/bas/4.jpg"
+            ),
+            new Product(
+                "Гальванические ванны и емкости",
+                "Уточнайте",
+                "Листовой полипропилен — экологически чистый, прочный и устойчивый к ультрафиолетовому излучению пластик",
+                "",
+                "http://aplast.com.ua/images/9.jpg"
+            ),
+            new Product(
+                "Жироуловители из полипропилена",
+                "Уточнайте",
+                "Листовой полипропилен — экологически чистый, прочный и устойчивый к ультрафиолетовому излучению пластик",
+                "",
+                "http://aplast.com.ua/images/bas/4.jpg"
+            ),
+            new Product(
+                "КНС",
+                "Уточнайте",
+                "Листовой полипропилен — экологически чистый, прочный и устойчивый к ультрафиолетовому излучению пластик",
+                "",
+                "http://aplast.com.ua/images/Nasos.png"
+            )
+        ];
 
     }
 }
-/**
- * Created by dima- on 31.07.2016.
- */
